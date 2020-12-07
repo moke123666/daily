8bf5371a46f0c8b6f674018a4ab0f7ce305c807f
moke <1805593886@qq.com>
2020-12-07 14:02:51 +0800

    first submission

--- app/src/main/java/club/mokeblog/diary/ui/fragment/DiaryFragment.java
+++ app/src/main/java/club/mokeblog/diary/ui/fragment/DiaryFragment.java
@@ -1,25 +1,17 @@
 package club.mokeblog.diary.ui.fragment;
 
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
 
 import club.mokeblog.diary.R;
 import club.mokeblog.diary.base.BaseFragment;
 
 public class DiaryFragment extends BaseFragment {
     
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-    }
-
     @Override
     protected int getRootViewId() {
-       return R.layout.fragment_diary;
+        return R.layout.fragment_diary;
     }
 
     @Override
